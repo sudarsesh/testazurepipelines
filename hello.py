40be331c15a3393874dc573b0955f71bd799007d
ssudarsh <ssudarsh@celestica.com>
2022-02-28 10:41:32 +0000

    writes to a log directory

--- hello.py
+++ hello.py
@@ -4,8 +4,13 @@ print("hello-world")
 
 now_str = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
 
-with open('hello.log', 'w') as logfile:
+if os.path.isdir('/tmp/test_logs') is False:
+    os.path.mkdir('/tmp/test_logs')
+
+log_path = "/tmp/test_logs"
+    
+with open(f'{log_path}/hello.log', 'w') as logfile:
     logfile.write(f"{now_str} sample log\n")
 
-with open('hello.log') as logfile:
+with open(f'{log_path}/hello.log') as logfile:
     print(logfile.read())

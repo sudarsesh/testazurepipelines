dc8cf0e1abd6164f75d1b2457ee8320737593142
ssudarsh <ssudarsh@celestica.com>
2022-02-28 09:56:16 +0000

    creates logs in logfile

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
+from datetime import datetime
+
 print("hello-world")
+
+now_str = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+
+with open('hello.log', 'w') as logfile:
+    logfile.write(f"{now_str} sample log\n")
+
+with open('hello.log') as logfile:
+    print(logfile.read())
